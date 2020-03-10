ca61b53b4b90b507293805f124f15a9c3c9390af
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-03-10 10:49:24 +0200

    Add missing files

---
+++ functions/__fzf_apt_install.fish
@@ -0,0 +1,21 @@
+function __fzf_apt_install -d "Install apt packages"
+    set -l commandline (__fzf_parse_commandline)
+    set -l fzf_query $commandline[2]
+
+    set -l preview_cmd
+    if set -q FZF_ENABLE_PREVIEW
+        set preview_cmd '--preview-window=right:wrap --preview=\'apt-cache show {1}\''
+    end
+
+    varcache APT_CACHE_SEARCH 'apt-cache search .' '12 hours' compressed | eval (__fzfcmd) $preview_cmd "-m $FZF_DEFAULT_OPTS --query \"$fzf_query\"" | while read -l s
+        set select $select (echo $s | cut -d' ' -f1)
+    end
+
+    if test (count $select) -gt 0
+        commandline "sudo apt-get install $select"
+        and commandline -f execute
+        set open_status $status
+    end
+    commandline -f repaint
+    return $open_status
+end
