fb5e3bfabb33c9416c73e664ff08500fe2268092
Alin Panaitiu <alin.panaitiu@gmail.com>
2020-11-16 17:32:36 +0200

    Update

--- functions/__with_cdpath.fish
+++ functions/__with_cdpath.fish
@@ -1,10 +1,14 @@
 function __with_cdpath
+    if test -d "$argv"; or test -d "./$argv"
+        echo "$argv"
+        return 0
+    end
+
     for dir in $CDPATH
         if test -d "$dir/$argv"
             echo "$dir/$argv"
             return 0
         end
     end
-    echo ''
-    return 1
+    echo "$argv"
 end
