71f525e6937230c184377c55edb57332010657af
Alin Panaitiu <alin.panaitiu@gmail.com>
2021-10-01 11:18:24 +0300

    reverse search

--- functions/__fzf_reverse_isearch.fish
+++ functions/__fzf_reverse_isearch.fish
@@ -1,6 +1,24 @@
-function __fzf_reverse_isearch
-    history merge
-    history -z | eval (__fzfcmd) --read0 --tiebreak=index --toggle-sort=ctrl-r $FZF_DEFAULT_OPTS $FZF_REVERSE_ISEARCH_OPTS -q '(commandline)' | perl -pe 'chomp if eof' | read -lz result
-    and commandline -- $result
-    commandline -f repaint
+function __fzf_reverse_isearch --description "Search command history. Replace the command line with the selected command."
+    # history merge incorporates history changes from other fish sessions
+    builtin history merge
+
+    set command_with_ts (
+        # Reference https://devhints.io/strftime to understand strftime format symbols
+        builtin history --null --show-time="%m-%d %H:%M:%S │ " |
+        eval (__fzfcmd) (string escape -- --read0 \
+            --tiebreak=index \
+            --query=(commandline) \
+            # preview current command using fish_ident in a window at the bottom 3 lines tall
+            --preview="echo {4..} | fish_indent --ansi" \
+            --preview-window="bottom:10:wrap" \
+            $fzf_history_opts) |
+        string collect
+    )
+
+    if test $status -eq 0
+        set command_selected (string split --max 1 " │ " $command_with_ts)[2]
+        commandline --replace -- $command_selected
+    end
+
+    commandline --function repaint
 end
