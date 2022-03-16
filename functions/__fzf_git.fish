6598e645c87147fe20b1d6b9384f4d8c5f44145f
Alin Panaitiu <alin.panaitiu@gmail.com>
2022-03-16 11:23:16 +0200

    Fzf git

--- functions/__fzf_git.fish
+++ functions/__fzf_git.fish
@@ -1,51 +1,51 @@
 function __fzf_is_in_git_repo
-  git rev-parse HEAD > /dev/null 2>&1
+    git rev-parse HEAD >/dev/null 2>&1
 end
 
 function __fzf_gf
-  __fzf_is_in_git_repo || return
-  git -c color.status=always status --short |
-  eval (__fzfcmd) -m --ansi --nth 2..,.. \
-    --preview (string escape -n -- 'git diff --color=always -- {-1} | sed 1,4d; if file --mime {-1} | grep -iq directory; then "(__fzf_dir_preview)" {-1}; elif file --mime {-1} | grep -iq binary; then if which hexyl 2>/dev/null; then hexyl -n 512 --color always {-1}; else xxd -l512 {-1}; fi; elif which bat 2>/dev/null; then bat --line-range :200 --paging never --color always {-1}; else head -n 30 {-1}; fi') |
-  cut -c4- | sed 's/.* -> //'
+    __fzf_is_in_git_repo || return
+    git -c color.status=always status --short |
+        eval (__fzfcmd) -m --ansi --nth 2..,.. \
+            --preview (string escape -n -- 'git diff --color=always -- {-1} | sed 1,4d; __fzf_universal_preview {-1}') |
+        cut -c4- | sed 's/.* -> //'
 end
 
 function __fzf_gb
-  __fzf_is_in_git_repo || return
-  git branch -a --color=always | grep -v '/HEAD\s' | sort |
-  eval (__fzfcmd) --ansi --multi --tac --preview-window right:70% \
-    --preview (string escape -n -- 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(echo {} | sed s/^..// | cut -d" " -f1) | head -n 200') |
-  sed 's/^..//' | cut -d' ' -f1 |
-  sed 's#^remotes/origin/##'
-  or sed 's#^remotes/##'
+    __fzf_is_in_git_repo || return
+    git branch -a --color=always | grep -v '/HEAD\s' | sort |
+        eval (__fzfcmd) --ansi --multi --tac --preview-window right:70% \
+            --preview (string escape -n -- 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(echo {} | sed s/^..// | cut -d" " -f1) | head -n 200') |
+        sed 's/^..//' | cut -d' ' -f1 |
+        sed 's#^remotes/origin/##'
+    or sed 's#^remotes/##'
 end
 
 function __fzf_gt
-  __fzf_is_in_git_repo || return
-  git tag --sort -version:refname |
-  eval (__fzfcmd) --multi --preview-window right:70% \
-    --preview (string escape -n -- 'git show --color=always {} | head -n 200')
+    __fzf_is_in_git_repo || return
+    git tag --sort -version:refname |
+        eval (__fzfcmd) --multi --preview-window right:70% \
+            --preview (string escape -n -- 'git show --color=always {} | head -n 200')
 end
 
 function __fzf_gh
-  __fzf_is_in_git_repo || return
-  git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
-  eval (__fzfcmd) --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
-    --header (string escape -n -- 'Press CTRL-S to toggle sort') \
-    --preview (string escape -n -- 'echo {} | grep -o "[a-f0-9]\{7,\}" | xargs git show --color=always | head -n 200') |
-  grep -o "[a-f0-9]\{7,\}"
+    __fzf_is_in_git_repo || return
+    git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
+        eval (__fzfcmd) --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+            --header (string escape -n -- 'Press CTRL-S to toggle sort') \
+            --preview (string escape -n -- 'echo {} | grep -o "[a-f0-9]\{7,\}" | xargs git show --color=always | head -n 200') |
+        grep -o "[a-f0-9]\{7,\}"
 end
 
 function __fzf_gr
-  __fzf_is_in_git_repo || return
-  git remote -v | awk '{print $1 "\t" $2}' | uniq |
-  eval (__fzfcmd) --tac \
-    --preview (string escape -n -- 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -n 200') |
-  cut -d\t -f1
+    __fzf_is_in_git_repo || return
+    git remote -v | awk '{print $1 "\t" $2}' | uniq |
+        eval (__fzfcmd) --tac \
+            --preview (string escape -n -- 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -n 200') |
+        cut -d\t -f1
 end
 
 function __fzf_git -d "Git with FZF"
-    argparse --name=fzf-git 'h/hash' 'r/remote' 't/tag' 'b/branch' 'f/file' 'e/echo' -- $argv
+    argparse --name=fzf-git h/hash r/remote t/tag b/branch f/file e/echo -- $argv
     or return
 
     set -l commandline (__fzf_parse_commandline)
